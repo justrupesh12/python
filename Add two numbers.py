487f6bb63e49330e513cb53219110cf339d5e7ab
JustRupesh12 <rk35386@gmail.com>
2024-08-27 18:20:37 +1000

    Add two number

--- Add two numbers.py
+++
@@ -1,8 +0,0 @@
-Python 3.12.4 (tags/v3.12.4:8e8a4ba, Jun  6 2024, 19:30:16) [MSC v.1940 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> # first program to Add two number
->>> a=10
->>> b=20
->>> c=a+b
->>> print("sum= ",c)
-sum=  30
